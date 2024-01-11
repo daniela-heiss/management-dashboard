ee0f29dd9971ff892c570a065bc102df1171174c
Daniel Braxmeier <inf3838@hs-worms.de>
2024-01-11 15:11:29 +0000

    Revert "Cleaned some Code and added Expected Rev next year"
    This reverts commit 8e926021bcf15164b125ec13469aae0ba80b55e0

--- app/routes/costumerRoutes.js
+++ app/routes/costumerRoutes.js
@@ -1,11 +1,11 @@
 import Router from 'express'
 import { getAllCustomers} from '../controller/select.js'
 import { getHighRevenue } from '../controller/select.js'
+import {cusWithMostOrders} from '../controller/select.js'
 
 const routerCustomer = Router();
 
 routerCustomer.get('/customer', getAllCustomers);
 routerCustomer.get('/customer_highest',getHighRevenue);
-
 
 export{routerCustomer}
