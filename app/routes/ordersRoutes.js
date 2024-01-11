8e926021bcf15164b125ec13469aae0ba80b55e0
I3aum <131444450+I3aum@users.noreply.github.com>
2024-01-11 16:07:20 +0100

    Cleaned some Code and added Expected Rev next year

--- app/routes/ordersRoutes.js
+++ app/routes/ordersRoutes.js
@@ -1,7 +1,9 @@
 import Router from 'express'
 import {cusWithMostOrders} from '../controller/select.js'
+import { getExpectedRev } from '../controller/select.js';
 
 
 const routerOrders = Router();
 routerOrders.get('/orders', cusWithMostOrders);
+routerOrders.get('/ordersExpect', getExpectedRev)
 export{routerOrders}
