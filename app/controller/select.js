31901e05c8f2a028d815e41cbd6ec2ae42533f86
Daniel Braxmeier <131444450+I3aum@users.noreply.github.com>
2024-01-12 14:25:13 +0100

    Refactored code and added getExpectedRev

--- app/controller/select.js
+++ app/controller/select.js
@@ -1,11 +1,6 @@
-import { response } from 'express'
 import {costumer} from '../models/customer.js'
 import {lineItem} from '../models/lineitem.js'
-import {nation} from '../models/nation.js'
 import {order} from '../models/orders.js'
-import {part} from '../models/part.js'
-import {partsupp} from '../models/partsupp.js'
-import {region} from '../models/region.js'
 import {supplier} from '../models/supplier.js'
 import { sequelize } from '../models/dbconnection.js'
 import { Sequelize } from 'sequelize'
@@ -65,58 +60,21 @@ async function getTotalRev(request,response,startDate, endDate){
     response.json(totalRevenues);
 }
 
+//Calculate last month Revenue
 async function getLastMonthRev(request,response){
     const lastMonthRev = await order.sum('O_TOTALPRICE')
     response.json(lastMonthRev);
 }
-// Find all lineItems
-async function getAllLineItems(request,response)
-{
-    const lineItems = await lineItem.findAll();
-    console.log("All LineItems:", JSON.stringify(lineItems, null, 2));
-    response.json(lineItems);
-}
 
-//Find all nations
-async function getAllNations(request,response)
-{
-    const nations = await nation.findAll();
-    console.log("All Nations:", JSON.stringify(nations, null, 2)); 
-    response.json(nations);
-}
 
 //Find all orders
-async function getAllOrders(request,response)
+async function getExpectedRev(request,response)
 {
-    const orders = await order.findAll();
-    console.log("All orders:", JSON.stringify(orders, null, 2));
-    response.json(orders);
+    const ExpectedRev = await order.sum('O_TOTALPRICE');
+    const MultipliedRev = ExpectedRev * 1.5;
+    response.json(MultipliedRev);
 }
 
-//Find all parts
-async function getAllParts(request,response)
-{
-    const parts = await part.findAll();
-    console.log("All Parts:", JSON.stringify(parts, null, 2));
-    response.json(parts);
-
-}
-//Find all partsupps
-async function getAllPartSupps(request,response)
-{
-    const partsupps = await partsupp.findAll();
-    console.log("All partsupps:", JSON.stringify(partsupps, null, 2));
-    response.json(partsupps);
-}
-
-//Find all region
-async function getAllRegions(request,response)
-{
-    const regions = await region.findAll();
-    console.log("All regions:", JSON.stringify(regions, null, 2));
-    response.json(regions);
-}
-//Find all supplier
 async function getAllSupplier(request, response)
 {
     const suppliers = await supplier.findAll();
@@ -125,10 +83,11 @@ async function getAllSupplier(request, response)
 }
 
 export{
-    getAllSupplier,
     getAllCustomers,
     getHighRevenue,
     cusWithMostOrders,
     getTotalRev,
-    getLastMonthRev
+    getLastMonthRev,
+    getExpectedRev,
+    getAllSupplier
 }
