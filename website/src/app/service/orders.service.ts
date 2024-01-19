75b21764251ad225b17349a642949668f855532e
Daniela Heiß <heiss_dani@yahoo.com>
2024-01-19 12:25:57 +0100

    Deletion of getHighestRevCustomer and getHighestOrderCustomer in the orderService

--- website/src/app/service/orders.service.ts
+++ website/src/app/service/orders.service.ts
@@ -15,16 +15,4 @@ export class OrdersService {
     console.log('execute listOrders');
     return this.httpClient.get<OrdersDTO[]>('http://localhost:8080/orders')
   }
-
-  public getHighestRevCustomer(startDate: string, endDate: string): Observable<OrdersDTO[]> {
-    console.log('execute get highest rev customer');
-    const options = {params: new HttpParams().set('startDate', startDate).set('endDate', endDate)};
-    return this.httpClient.get<OrdersDTO[]>('http://localhost:8080/customer_highest', options);
-  }
-
-  public getHighestOrdersCustomer(startDate: string, endDate: string): Observable<OrdersDTO[]> {
-    console.log('execute get highest rev customer');
-    const options = {params: new HttpParams().set('startDate', startDate).set('endDate', endDate)};
-    return this.httpClient.get<OrdersDTO[]>('http://localhost:8080/orders/most', options);
-  }
 }
